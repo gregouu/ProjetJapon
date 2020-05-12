ff32b8f50ae943f887ebb63385c1fc74c28e101e
gregouu <62592125+gregouu@users.noreply.github.com>
2020-05-12 06:51:16 +0200

    Update membre_galerie.php

--- membre_galerie.php
+++ membre_galerie.php
@@ -1,12 +1,53 @@
-<!DOCTYPE html>
-<html lang="en">
+ <?php
+session_start ();
+;?>
+
+
+
+<html lang="fr" xmlns:og="http://ogp.me/ns#" xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <meta property="og:url" content="https://www.projetjapon.com"> <!--opne graph-->
+    <meta property="og:title" content="THE artist's">
+    <meta property="og:description" content="Venez admierer des travaux plus qu'incroyable de plusieurs artistes">
+    <meta property="og:image" content="https://journalmetro.com/wp-content/uploads/2019/11/iStock-1166277130-e1574864225841.jpg?w=860">
+    
+   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> <!-- pour empecher le clic droit-->
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+   
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script><!--la loupe-->
+    <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+    
+    <link rel="icon" type="image/png" href="images/favicon.png" />
+    
+    <script src="https://kit.fontawesome.com/ebfafc2eb8.js" crossorigin="anonymous"></script>
+    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/artistes.css">
+    <link rel="stylesheet" href="css/footer.css">
+
+   <title>THE artist's</title>
+    <script type="text/javascript">
+    //<!--
+        document.oncontextmenu = new Function("return false");
+    //-->
+    </script> <!-- empecher le clic droit-->
 </head>
 <body>
-   
-      
+  
+
+<a id="button"></a>
+
+<?php include 'include/header.php';?>
+
+   <br><br>
+                   <div class="container">
+                   <div class="row">  
      <?php 
     $dos = "images_/min";
     $dir = opendir($dos);
@@ -15,12 +56,22 @@
         $ext =  strtolower(substr($files, -3));
         if(in_array($ext, $allow_ext)){
             ?>
-                <img src="images_/min/<?php echo $files; ?>" />
+               
+
+                       <div class="col-3">
+                           <img src="images_/min/<?php echo $files; ?>" />
+                       </div>
+
                 
             <?php   
         }
     }
     ;?>
     
-</body>
-</html>
+                       </div>
+               </div>
+                
+    
+
+    <br><br>
+<?php include 'include/footer.php';?>
